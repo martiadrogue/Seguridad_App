a4db46a7a9cd7b88b422eb4581464a5e294c935c
marti adrogue <marti.adrogue@gmail.com>
2015-06-01 19:10:38 +0200

    New email sistem.

--- src/Common/MailSender.php
+++ src/Common/MailSender.php
@@ -2,21 +2,45 @@
 
 namespace Common;
 
+use PHPMailer;
+
 class MailSender
 {
+    private $mail;
+
+    public function __construct() {
+        $this->mail = new PHPMailer;
+
+        $this->mail->isSMTP();
+        $this->mail->Host = 'smtp.gmail.com';
+        $this->mail->SMTPAuth = true;
+        $this->mail->Username = 'mpwar.seguretat@gmail.com';
+        $this->mail->Password = 'seguretat';
+        $this->mail->SMTPSecure = 'tls';
+        $this->mail->Port = 587;
+        $this->mail->From = 'mpwar.seguretat@gmail.com';
+        $this->mail->FromName = 'Seguretat Admin';
+    }
+
     public function sendMailRegisterVerification($para, $hash)
     {
-        $titulo    = 'Activacion cuenta usuario en seguridad.dev';
-        $mensaje   = 'Hola, ya casi hemos terminado, para activar tu cuenta en Seguridad.dev sólo debes hacer click en el siguiente enlace. Ten en cuenta que si no lo haces no podrás entrar en tu cuenta  http://www.seguridad.dev/accountverification/' . $hash;
-        $cabeceras = 'From: webmaster@example.com' . "\r\n" . 'Reply-To: webmaster@example.com' . "\r\n" . 'X-Mailer: PHP/' . phpversion();
-        mail($para, $titulo, $mensaje, $cabeceras);
+        $this->mail->addAddress($para);
+        $this->mail->isHTML(true);
+        $this->mail->Subject = 'Activacion cuenta usuario en seguridad.dev';
+        $this->mail->Body = 'Hola, ya casi hemos terminado, para activar tu cuenta en Seguridad.dev sólo debes hacer click en el siguiente enlace. Ten en cuenta que si no lo haces no podrás entrar en tu cuenta  http://www.seguridad.dev/accountverification/' . $hash;
+        $this->mail->AltBody = 'Hola, ya casi hemos terminado, para activar tu cuenta en Seguridad.dev sólo debes hacer click en el siguiente enlace. Ten en cuenta que si no lo haces no podrás entrar en tu cuenta  http://www.seguridad.dev/accountverification/' . $hash;
+
+        $this->mail->send();
     }
 
     public function sendMailPasswordRecovery($para, $hash)
     {
-        $titulo    = 'Recuperación contraseña en seguridad.dev';
-        $mensaje   = 'Hola, ya casi hemos terminado, para cambiar tu password en Seguridad.dev sólo debes hacer click en el siguiente enlace.  http://www.seguridad.dev/changepassword/' . $hash;
-        $cabeceras = 'From: webmaster@example.com' . "\r\n" . 'Reply-To: webmaster@example.com' . "\r\n" . 'X-Mailer: PHP/' . phpversion();
-        mail($para, $titulo, $mensaje, $cabeceras);
+        $this->mail->addAddress($para);
+        $this->mail->isHTML(true);
+        $this->mail->Subject = 'Recuperación contraseña en seguridad.dev';
+        $this->mail->Body    = 'Hola, ya casi hemos terminado, para cambiar tu password en Seguridad.dev sólo debes hacer click en el siguiente enlace.  http://www.seguridad.dev/changepassword/' . $hash;
+        $this->mail->AltBody    = 'Hola, ya casi hemos terminado, para cambiar tu password en Seguridad.dev sólo debes hacer click en el siguiente enlace.  http://www.seguridad.dev/changepassword/' . $hash;
+
+        $this->mail->send();
     }
 }
